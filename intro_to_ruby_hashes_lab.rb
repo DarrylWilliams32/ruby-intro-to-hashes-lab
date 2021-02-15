9b1c3888b56fe3e30b3daf738bb290039266ad7f
darryl_williams <darrylwilliams6@gmail.com>
2021-02-14 20:47:52 -0500

    commit tree 8aa0115f2548c94b5abdcf787e828744875e4ec1

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,37 @@
+#create new empty hash and return it
 def new_hash
-
+new_hash={}
 end
+new_hash 
 
+#create has named 'actor' w/ one key-value pair. Key is a symbol.  Name= "Dwayne the Rock Johnson"
 def actor
-
+actor={name: "Dwayne The Rock Johnson"}
 end
 
+#adds a symbol equal to an empty hash to the monopoly empty hash
 def monopoly
-	monopoly = {}
-
+monopoly={
+	:railroads=>{}
+}
 end
-
+#adds a symbol inside the railroads symbol. sets symbol equal to integer 4
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly={
+		:railroads=>
+		{
+			:pieces=> 4
+			:names=>{}
+	}
+}
+end
+
+#adds a key-value pair to second level of monopoly hash. key is a symbol of 'name' which is equal to an empty hash
+	def monopoly_with_third_tier
 
 end
 
-def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
-end
-
+#adds in another hash equal to rent_in_dollars that commes after the 'pieces' hash
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+	#copy and paste the monopoly hash you build in the previous method. We're going to add stuff to it here!
 end
